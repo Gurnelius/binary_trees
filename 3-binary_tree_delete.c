8cb36ea54c016b04523d827c3fa8be2163dcac81
Gurnelius <terergurnelius@gmail.com>
2024-02-26 15:47:50 +0300

    Initial commit

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,1 +1,21 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_delete - delete the entire binary tree
+ *
+ * @tree: pointer to the binary tree
+ *
+ * Return: nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+	{
+		free(tree);
+		return;
+	}
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
