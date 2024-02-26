5f3250e1a34283663fb85d35221eed0890f875f0
Gurnelius <terergurnelius@gmail.com>
2024-02-26 17:28:57 +0300

    Initial commit

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,1 +1,20 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_size - measure the size of a binary tree
+ * @tree: Pointer to the root of the binary tree
+ *
+ * Return: size of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size = 0;
+	size_t right_size = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+
+	return ((left_size + right_size) + 1);
+}
