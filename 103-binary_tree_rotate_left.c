15a81540168e18a4b409e614001a9a08845dace3
Gurnelius <terergurnelius@gmail.com>
2024-02-29 13:48:09 +0300

    Initial commit

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_rotate_left - performs a left-rotation
+ * on a binary tree
+ *
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: pointer to the new root node.
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
